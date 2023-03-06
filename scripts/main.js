88462ce8047546026e3445a899088fe7a1b4b99a
Vivek <codervivekk@gmail.com>
2023-03-06 11:15:44 +0530

    my first app

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,26 @@
-let Name, admin;
+// let a=prompt();
+// let b=a;
+// console.log(b);
 
-Name="John";
-admin=Name;
 
-alert(Name);
+let user,password;
+user=prompt("Who's there?");
+
+if(user=="Admin"){
+    password=prompt("Enter Password:")
+    if(password=="TheMaster"){
+        alert("Welcome!")
+    }
+    else if(password==null || password==undefined){
+        alert("Cancelled!");
+    }
+    else{
+        alert("Wrong Password!");
+    }
+}
+else if(user==null || user==undefined){
+    alert("Cancelled!");
+}
+else{
+    alert("I don't know you! ");
+}
